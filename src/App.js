d1257b3e7599ffd6509624477e0e889d4c95f4a9
workdai07 <work.dai07@gmail.com>
2025-05-20 23:25:40 +0700

    feat: userMutation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from './api';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { fetchPosts, createPost  } from './api';
+import { useState } from 'react';
 
 function App() {
-  const { data, isLoading, isError } = useQuery({
+
+  const queryClient = useQueryClient();
+  const [title, setTitle] = useState('');
+
+  const { data: posts, isLoading, isError } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
   });
+
+  const mutation = useMutation({
+    mutationFn: createPost,
+    onSuccess: (newPost) => {
+      queryClient.setQueryData(['posts'], (oldPosts) => [newPost, ...oldPosts]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+    mutation.mutate({ title, body: 'Demo content', userId: 1 });
+    setTitle('');
+  };
 
   if (isLoading) 
     return <h1>Loading data...</h1>;
@@ -15,8 +34,21 @@ function App() {
   return (
     <div className="App">
       <h1>List status</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          placeholder="Status title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit" disabled={mutation.isPending}>
+          {mutation.isPending ? 'Loading...' : 'Create new status'}
+        </button>
+      </form>
+
       <ul>
-        {data.slice(0, 10).map((post) => (
+        {posts.slice(0, 10).map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
--- src/api.js
+++ src/api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 export const fetchPosts = async () => {
-  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const res = await axios.get(API_URL);
   return res.data;
 };
+
+export const createPost = async (newPost) => {
+    const res = await axios.post(API_URL, newPost);
+    return res.data;
+  };
